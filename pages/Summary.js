ffc62699d2a7eb726b5bec08ff94a39c5425f189
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-06 14:09:38 +0530

    Update Summary.js

--- pages/Summary.js
+++ pages/Summary.js
@@ -1,15 +1,43 @@
-'use client';
-import { useState } from 'react';
+// 'use client';
+// import { useState } from 'react';
+// function Summary({ formData }) {
+//   console.log("form data ",formData);
+//   return (
+//     formData.step===2?
+//     (<div id='summary'>
+//       <h2>Summary</h2>
+      
+//       <p>Form Type: Form A</p>
+//       <p>Age:{formData.age} </p>
+
+//       <p>DC Shows:{formData.dc} </p>
+      
+//     </div>):(<div id='summary'>
+//       <h2>Summary</h2>
+      
+//       <p>Form Type: Form B</p>
+//       <p>Age:{formData.age} </p>
+//       <p>Marvel Shows:{formData.marvelShows} </p>
+      
+//     </div>)
+
+//   );
+// }
+// export default Summary;
+
+"use client";
+import { useState } from "react";
 function Summary({ formData }) {
   return (
-    <div id='summary'>
+    <div id="summary">
       <h2>Summary</h2>
-      <p>Form Type: </p>
-      <p>Age: </p>
-
-      <p>DC Shows: </p>
-
-      <p>Marvel Shows: </p>
+      <p>Form Type: {formData.type}</p>
+      <p>Age: {formData.age}</p>
+      {formData.dcShows ? (
+        <p>DC Shows: {formData.dcShows}</p>
+      ) : (
+        <p>Marvel Shows: {formData.marvelShows}</p>
+      )}
     </div>
   );
 }
